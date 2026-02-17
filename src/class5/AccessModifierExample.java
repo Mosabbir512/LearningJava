0f07393baa0113d87ba1406d9c4ee16ec194d9e6
Mosabbir512 <mosabbirhossain512@gmail.com>
2026-02-17 12:53:12 +0600

    java assignment three

--- src/class5/AccessModifierExample.java
+++ src/class5/AccessModifierExample.java
@@ -1,4 +1,35 @@
 package class5;
 
 public class AccessModifierExample {
+
+    public int publicVar = 10;
+    protected int protectedVar = 20;
+    int defaultVar = 30;   // default access
+    private int privateVar = 40;
+
+    public void showValues() {
+        System.out.println("Public: " + publicVar);
+        System.out.println("Protected: " + protectedVar);
+        System.out.println("Default: " + defaultVar);
+        System.out.println("Private: " + privateVar);
+    }
+
+    public static void main(String[] args) {
+        AccessModifierExample obj = new AccessModifierExample();
+        obj.showValues();
+    }
+}
+
+
+class Test {
+    public static void main(String[] args) {
+
+        AccessModifierExample obj = new AccessModifierExample();
+
+        System.out.println("Public: " + obj.publicVar);
+        System.out.println("Protected: " + obj.protectedVar);
+        System.out.println("Default: " + obj.defaultVar);
+
+         System.out.println("Private modifier is eiligible");
+    }
 }
